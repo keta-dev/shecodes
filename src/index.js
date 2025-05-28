766a436c27b01374910481f209bf53f5c4d66cd7
keta-dev <89269904+keta-dev@users.noreply.github.com>
2025-05-28 13:19:11 +0100

    enabled axios

---
+++ src/index.js
@@ -0,0 +1,64 @@
+// import axios from "axios";
+
+const apiKey = "07678d78bd5438a79f8d4tc414d2o39e";
+
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#current-city");
+  let city = searchInputElement.value.trim();
+
+  if (city) {
+    const apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiURL).then((response) => {
+      const data = response.data;
+      console.log("get data", data);
+
+      // Update City Name
+      cityElement.innerHTML = `${data.city}, ${data.country}`;
+
+      // Update City Temperature
+      const temperature = Math.round(data.temperature.current);
+      const temperatureElement = document.querySelector(
+        ".current-temperature-value"
+      );
+
+      temperatureElement.innerHTML = temperature;
+    });
+  }
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+
+let currentDateELement = document.querySelector("#current-date");
+let currentDate = new Date();
+
+currentDateELement.innerHTML = formatDate(currentDate);
